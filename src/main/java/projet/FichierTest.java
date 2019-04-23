e4c1189991731434b18260ccfdc87125d5c0e93d
nfndame <nfndame@gmail.com>
2019-04-23 12:26:38 +0200

    Creation  de classe fichier et la methode de recherche

---
+++ src/main/java/projet/FichierTest.java
@@ -0,0 +1,25 @@
+package projet;
+
+import projet.Fichier;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FichierTest 
+{
+
+	public static void main(String[] args) throws IOException 
+        {
+		// TODO Auto-generated method stub
+	Fichier fich = new Fichier();
+		fich.edit("test1.adoc");
+               fich.edit("test2.adoc");
+        //  fich.search("bonjour");
+              
+	}
+
+ 
+    }
+
+
--- src/main/java/projet/Rechercher.java
+++ src/main/java/projet/Rechercher.java
@@ -9,7 +9,6 @@ public class Rechercher implements Command {
 	  public void execute(String mot){
 	    this.fonction.Rechercher(mot); ;
 	  }
-
-
-
+	  
+	  
 }
